1b824e0ed657a636331ccbf9f622d0101d510e66
Robin Eidissen <robinei@gmail.com>
2017-02-28 22:21:30 +0100

    dedicate 25% of lru cache to single use elements to avoid trashing

--- LruCache.java
+++ LruCache.java
@@ -7,49 +7,60 @@ public class LruCache<K,V> {
         V value;
         Entry<K,V> prev;
         Entry<K,V> next;
+        List<K,V> list;
 
         Entry(K key) {
             this.key = key;
         }
 
         void unlink() {
-            if (prev == this) {
-                throw new RuntimeException("can't unlink from empty list");
-            }
+            assert prev != null;
+            assert next != null;
+            assert prev != this;
+            assert next != this;
             prev.next = next;
             next.prev = prev;
             prev = null;
             next = null;
+            --list.size;
+            list = null;
         }
     };
 
     private static class List<K,V> extends Entry<K,V> {
-        public List() {
+        int size;
+
+        List() {
             super(null);
             prev = next = this;
         }
 
         void clear() {
-            prev = next = this;
+            while (next != this) {
+                next.unlink();
+            }
         }
         
         void append(Entry<K,V> e) {
+            assert e.next == null;
+            assert e.prev == null;
             e.prev = prev;
             e.next = this;
             prev.next = e;
             prev = e;
+            e.list = this;
+            ++size;
         }
 
         Entry<K,V> first() {
-            if (next == this) {
-                throw new RuntimeException("list is empty");
-            }
+            assert next != this;
             return next;
         }
     }
 
     private final HashMap<K, Entry<K,V>> entries = new HashMap<>();
-    private final List<K,V> list = new List<K,V>();
+    private final List<K,V> singleUse = new List<K,V>();
+    private final List<K,V> multiUse = new List<K,V>();
     private final int maxSize;
 
     public LruCache(int maxSize) {
@@ -62,33 +73,34 @@ public class LruCache<K,V> {
 
     public void clear() {
         entries.clear();
-        list.clear();
+        singleUse.clear();
+        multiUse.clear();
     }
 
     public void put(K key, V value) {
         Entry<K,V> e = entries.get(key);
         if (e == null) {
             if (entries.size() == maxSize) {
-                Entry<K,V> victim = list.first();
-                if (entries.remove(victim.key) != victim) {
-                    throw new RuntimeException("integrity error");
-                }
+                Entry<K,V> victim = singleUse.size >= maxSize / 4 ?
+                                        singleUse.first() : multiUse.first();
+                entries.remove(victim.key);
                 victim.unlink();
             }
             e = new Entry<K,V>(key);
             entries.put(key, e);
+            singleUse.append(e);
         } else {
             e.unlink();
+            multiUse.append(e);
         }
         e.value = value;
-        list.append(e);
     }
 
     public V get(K key) {
         Entry<K,V> e = entries.get(key);
         if (e != null) {
             e.unlink();
-            list.append(e);
+            multiUse.append(e);
             return e.value;
         }
         return null;
